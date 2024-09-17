2b88e477cc90335346ffd43f724a7a6f358686f3
guilherme <vinespedro55@gmail.com>
2024-09-17 11:45:20 -0300

    feat: model category

--- src/category/entities/models.py
+++ src/category/entities/models.py
@@ -1,22 +1,42 @@
-from dataclasses import dataclass
 import uuid
 
-@dataclass
-class Category:
+from pydantic import field_validator , BaseModel  , model_validator
+from src.category.entities.exceptions import erros
+
+
+
+class Category(BaseModel):
     id: uuid.UUID
     name: str
 
-    def validate(self)->None:
-        if not self.name:
-            raise ValueError("Name is required")
-        if len(self.name) > 100:
-            raise ValueError("Name should be less than 100 characters")
-        if not isinstance(self.name, str):
-            raise ValueError("Name should be a string")
-        if not self.id:
-            raise ValueError("Id is required")
-        if not isinstance(self.id, uuid.UUID):
-            raise ValueError("Id should be a UUID")
-        
-    def __post_init__(self):
-        self.validate()
+    def __str__(self):
+        return f'Category: id={self.id}, name={self.name}'
+
+    @model_validator(mode='before')
+    @classmethod
+    def check_based_info(cls,data: dict)-> dict:
+        if not data.get('id'):
+            raise erros.IdIsRequired
+        if not isinstance(data.get('id'), uuid.UUID):
+            raise erros.IdShouldBeUUID
+        if not data.get('name'):
+            raise erros.NameIsRequired
+        if not isinstance(data.get('name'), str):
+            raise erros.NameShouldBeString
+        return data
+
+    @field_validator('name')
+    @classmethod
+    def validate_name(cls, value: str):
+        if len(value) > 150:
+            raise erros.NameShouldBeLessThan150Characters
+        return value
+    
+    
+
+    
+
+
+
+
+
