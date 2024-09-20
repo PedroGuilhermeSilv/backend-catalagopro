5d8ab644c8e1c1af8223e90eb18c85549d87c182
guilherme <vinespedro55@gmail.com>
2024-09-20 14:51:28 -0300

    fix: lint

--- src/framework/urls.py
+++ src/framework/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+
 from src.core.security.infra.django.security.application.controller.login import (
     router as login_router,
 )
